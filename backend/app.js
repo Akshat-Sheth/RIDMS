0a5532239ad7794a8e74e5bb499b36ff3da8aa41
Akshat Sheth <akshatsheth2310@gmail.com>
2024-01-08 22:14:50 +0530

    first commit- started with the backend

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+
+// Config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({ path: "backend/config/config.env" });
+}
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+module.exports = app;
