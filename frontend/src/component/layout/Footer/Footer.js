ab4f6d9a8ef6b41b7bf991c7224e1049dac6867b
Akshat Sheth <akshatsheth2310@gmail.com>
2024-02-17 14:17:35 +0530

    actions added and setup the store

---
+++ frontend/src/component/layout/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer id="footer">
+
+      <div className="midFooter">
+        <h1>RIDMS</h1>
+        <p>&copy;Copyrights 2024</p>
+      </div>
+
+    </footer>
+  );
+};
+
+export default Footer;
